37a0a49e250c868cff1c430a4db47e3eca5283d3
Michael Church <michael@fishinglounge.com>
2015-08-10 12:57:51 -0400

    progress dev

--- wp-content/themes/sage/templates/header.php
+++ wp-content/themes/sage/templates/header.php
@@ -1,12 +1,13 @@
 <header class="banner" role="banner">
-  <div class="container">
-  	<p style="color:#fff">Logo, Contact link, Back to socrata.com</p>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 header">
+				<a class="white-logo header-logo" href="<?php echo home_url('/'); ?>"></a>
+				<ul class="ctas">
+					<li><a href="#learn-more" class="btn btn-primary">Learn More</a></li>
+					<li><a href="http://socrata.com">socrata.com</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </header>
